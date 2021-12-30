2a8f454e89461adf70b472a9ea42359f58996c2b
pwyoung <phil.w.young@gmail.com>
2021-12-30 00:01:46 -0500

    wip. rebase this branch

--- tests/unit-tests/pyalgofetcher/feed-scripts/pandas-datareader/README.md
+++
@@ -1,7 +0,0 @@
-# Goal
-This directory holds scripts related to loading data fetched via the pandas-datareader API
-
-# References
-Docs for this module are ... not easy to find.
-- Git code: https://github.com/pydata/pandas-datareader
-- PDF online: https://buildmedia.readthedocs.org/media/pdf/pandas-datareader/latest/pandas-datareader.pdf
--- tests/unit-tests/pyalgofetcher/pyalgofetcher.py
+++ tests/unit-tests/pyalgofetcher/pyalgofetcher.py
@@ -3,8 +3,10 @@
 import os
 import sys
 import argparse
-from lib.shellcmd import run_command
 import logging
+import yaml
+import datetime
+import pandas_datareader as web
 
 
 class Pyalgofetcher:
@@ -12,71 +14,98 @@ class Pyalgofetcher:
         # Arguments
         self.config_file = cfg.config_file  # Config file
         self.history_dir = cfg.history_dir  # Output of this project
-        self.feed_name = cfg.feed  # Feed to run
+        self.feed_name = cfg.feed  # Feed(s) to run
         logging.debug("config_file = " + self.config_file)
         logging.debug("history_dir = " + self.history_dir)
         logging.debug("feed_name = " + self.feed_name)
-        # Calculate some parameters
-        self.feed_dir = self.set_feed_dir()
-        logging.debug("feed_dir = " + self.feed_dir)
-
-    def set_feed_dir(self):
-        """ Specify for the Feed Implementation to run
-            Errors out if the feed directory is not found.
-        """
-        local_feed = os.path.normpath(os.path.join(os.getcwd(), "feed-scripts", self.feed_name))
-
-        feed_dir = None
-        if os.path.isdir(local_feed):
-            feed_dir = local_feed
-        else:
-            logging.warning("local feed directory does not exist: " + local_feed)
-
-        if feed_dir is None:
-            logging.critical("Could not find directory for feed: " + self.feed_name)
-            sys.exit(1)
-
-        if not os.path.isdir(feed_dir):
-            logging.critical("feed_dir is not a directory: " + feed_dir)
-            sys.exit(1)
-
-        return feed_dir
-
-    def execute_feed(self):
-        """ Execute the specified feed
-        """
-        # The Algorithm is:
-        #   - Create and cd into working directory "<HISTORY_DIR>/<feed>"
-        #   - Execute "<feed_dir>/<feed_script>"
-
         # Create the history_dir (if it doesn't exist)
         if not os.path.isdir(self.history_dir):
             os.mkdir(self.history_dir)
+        # Read the config file. It should be in the dir as this script
+        if not os.path.isfile(self.config_file):
+            logging.error("Config file is missing: " + self.config_file)
+            sys.exit(1)
+        with open(self.config_file, "r") as yamlfile:
+            self.cfg_data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+            logging.info("Read successful")
+        logging.info(self.cfg_data)
+        # start and end of processing
+        self.start_date = cfg.start_date
+        self.end_date = cfg.end_date
 
-        # Create the Working Directory for the feed
-        working_dir = os.path.normpath(os.path.join(self.history_dir,
-                                                    self.feed_name))
-        if not os.path.isdir(working_dir):
-                os.mkdir(working_dir)
-        # Traverse this way in case we change the way working_dir is set
-        conf_file = os.path.normpath(os.path.join(working_dir,
-                                                  "../..",
-                                                  "config.yaml"))
-        if not os.path.isfile(conf_file):
-            logging.error("Config file is missing: " + conf_file)
+
+    def process_pandas_datareader_feed(self, feed):
+        # Ensure the directory to hold the data for this feed exists
+        type_dir = os.path.normpath(os.path.join(self.history_dir, "pandas_datareader"))
+        if os.path.isdir(type_dir):
+            logging.debug("History Type directory for feed:" + feed + " exists at:" + type_dir)
+        else:
+            logging.debug("Creating history type directory for feed:" + feed + " at:" + type_dir)
+            os.mkdir(type_dir)
+        #
+        hist_dir = os.path.normpath(os.path.join(type_dir, feed))
+        if os.path.isdir(hist_dir):
+            logging.debug("History directory for feed:" + feed + " exists at:" + hist_dir)
+        else:
+            logging.debug("Creating history directory for feed:" + feed + " at:" + hist_dir)
+            os.mkdir(hist_dir)
+
+        logging.info("Loading pandas_datareader type feed:" + feed)
+        args = self.cfg_data['feeds'][feed]['args']
+        logging.debug("Args:" + str(args))
+        source = args['source']
+        symbol = args['symbol']
+
+        # datetime.datetime is a data type within the datetime module
+        #start = datetime.datetime(1990, 1, 1)
+        #start = datetime.datetime(2020, 12, 1) # fails since no data
+        #end = datetime.datetime(2021, 12, 18)
+        #
+        # https://docs.python.org/3/library/datetime.html#datetime.datetime.fromisoformat
+        #start = datetime.datetime.fromisoformat('1990-01-01')
+        #end = datetime.datetime.fromisoformat('2021-12-18')
+        start = datetime.datetime.fromisoformat(self.start_date)
+        end = datetime.datetime.fromisoformat(self.end_date)
+
+        # If there is no data for the date range given, this throws a "KeyError" on 'Date'
+        try:
+            df = web.DataReader(symbol, source, start, end)
+        except KeyError as e:
+            logging.error("No data found. Got KeyError:" + str(e))
+            logging.critical("No data found for feed:" + feed + " for range: " + self.start_date + " to " + self.end_date)
             sys.exit(1)
 
-        # Each directory should have a script with this name
-        feed_script = "process_feed.py"
+        rel_filename = "pandas_datareader_" + source + "_" + symbol + "_" + self.start_date + "_" + self.end_date + ".csv"
+        abs_filename = os.path.normpath(os.path.join(hist_dir, rel_filename))
+        logging.info("Writing data to:" + abs_filename)
+        df.to_csv(abs_filename)
 
-        command = os.path.normpath(os.path.join(self.feed_dir,
-                                                feed_script))
-        cmd = "cd " + working_dir + " && " + command
-        logging.info("Executing command: " + cmd)
-        rc = run_command(cmd)
-        if rc != 0:
-            logging.error("Command failed")
-            sys.exit(rc)
+    def process_feed(self, feed):
+        """ Process the given feed
+        """
+        feed_cfg = self.cfg_data["feeds"][feed]
+        logging.debug("Feed cfg: " + str(feed_cfg))
+        feed_type = feed_cfg['type']
+        if feed_type == 'pandas_datareader':
+            self.process_pandas_datareader_feed(feed)
+        else:
+            logging.info("Feed:" + feed + " has an invalid type:" + feed_type)
+
+    def run(self):
+        """ Run the feed loaders. If the given feed is "ALL" then load all of them.
+        """
+        if self.feed_name == "ALL":
+            logging.info("Process all feeds")
+        else:
+            logging.info("Processing just feed: " + self.feed_name)
+
+        feeds = self.cfg_data['feeds']
+        logging.debug("Feeds: " + str(feeds))
+        for feed in feeds:
+            logging.debug("Feed: " + str(feed))
+            if feed == self.feed_name or self.feed_name == 'ALL':
+                logging.debug("Processing feed with name: " + feed)
+                self.process_feed(feed)
 
 
 def read_cli_args(argv):
@@ -89,6 +118,8 @@ def read_cli_args(argv):
     config_file = os.path.normpath(os.path.join(cur_dir, "config.yaml"))
     history_dir = os.path.normpath(os.path.join(cur_dir, "feed-history"))
     feed = 'ALL'
+    start_date='1990-01-01'
+    end_date='2030-01-01'
 
     # Parse the args
     parser = argparse.ArgumentParser()
@@ -111,6 +142,16 @@ def read_cli_args(argv):
                         help='Path to the output directory for this project. Default:'
                              + history_dir,
                         default=history_dir)
+    parser.add_argument('-s', '--start-date',
+                        action='store', type=str, dest="start_date",
+                        help='Start date to fetch data. Default:'
+                             + start_date,
+                        default=start_date)
+    parser.add_argument('-e', '--end-date',
+                        action='store', type=str, dest="end_date",
+                        help='End date to fetch data. Default:'
+                             + end_date,
+                        default=end_date)
 
     args = parser.parse_args()
     return args
@@ -129,7 +170,7 @@ def main(argv):
     app_cfg = read_cli_args(argv)
     configure_logging(app_cfg)
     app = Pyalgofetcher(app_cfg)
-    app.execute_feed()
+    app.run()
 
 
 if __name__ == "__main__":
